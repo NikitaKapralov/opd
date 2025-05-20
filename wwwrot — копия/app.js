85f3214397c05bf07a9f4921e71ce2c5ea1f3f21
Zloy Gus <119751891+NikitaKapralov@users.noreply.github.com>
2025-05-20 22:12:36 +0500

    Add files via upload

--- wwwrot — копия/app.js
+++ wwwrot — копия/app.js
@@ -33,3 +33,16 @@ document.addEventListener('DOMContentLoaded', function() {
       const newSrc = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d33193.6505958905!2d60.56900037621643!3d56.83571661688658!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43c16e8ea8b8589f%3A0x4bdc777e135a8eb1!2z0KbQtdC90YLRgCwg0JXQutCw0YLQtdGA0LjQvdCx0YPRgNCzLCDQodCy0LXRgNC00LvQvtCy0YHQutCw0Y8g0L7QsdC7Lg!5e1!3m2!1sru!2sru!4v1746274986905!5m2!1sru!2sru&ll=56.8386,60.6055&z=16"; 
       iframe.src = newSrc;
     });
+
+    // app.js
+document.addEventListener('DOMContentLoaded', function() {
+  // JavaScript-код для инициализации Leaflet карты
+  var map = L.map('map').setView([56.8386, 60.6055], 13); // Екатеринбург
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; OpenStreetMap'
+  }).addTo(map);
+
+  // Здесь можно добавлять маркеры и другой код для карты
+});

7cffcac50a101f46181a1ee6c079c95a05100a5e
Арсений <arsunik07@mail.ru>
2025-06-18 15:30:11 +0500

    Починил ДБ

--- EkbCulture.AppHost/Migrations/20250618102724_FirstMigration.Designer.cs
+++ EkbCulture.AppHost/Migrations/20250618102724_FirstMigration.Designer.cs
@@ -3,6 +3,7 @@ using EkbCulture.AppHost.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EkbCulture.AppHost.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250618102724_FirstMigration")]
+    partial class FirstMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EkbCulture.AppHost/Migrations/20250618102724_FirstMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EkbCulture.AppHost.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Icon",
+                table: "Users",
+                newName: "IconUrl");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IconUrl",
+                table: "Users",
+                newName: "Icon");
+        }
+    }
+}
